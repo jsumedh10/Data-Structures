06ac68e65ce45b9b1deae277aa78e9b43d59267a
SumedhMJ <jsumedh10@gmail.com>
2017-08-09 00:02:49 +0530

    added new lines

--- sequential_search.py
+++ sequential_search.py
@@ -1,3 +1,17 @@
-print("Hi Sumedh")
-print("This is seconde line")
-print("line added after git checkout")
+# input is a list and a number to search in that list
+# output index if number is in list otherwise, item not found
+
+def seq_search(lst, item):
+	index = 0
+	found = False
+	while index < len(lst) and not found:
+		if lst[index] == item:
+			found = True
+		else:
+			index += 1
+			
+	return found
+	
+lst = [1,2,3,4,5,6,7,8,9]
+print(seq_search(lst, 5))
+print(seq_search(lst, 10))
